62e24f02f41d0f56b7b7c8fa884e101e15339ed2
Isabelly Cristina <isabelly.c.sales@aluno.senai.br>
2026-07-29 11:04:49 -0300

    diversas alterações

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,10 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { CalculadoraComponent } from './calculadora/calculadora';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  standalone: true,
+  imports: [CalculadoraComponent],
+  template: '<app-calculadora></app-calculadora>'
 })
-export class App {
-  protected readonly title = signal('calculadora');
-}
+export class AppComponent {}
--- src/app/calculadora/calculadora.ts
+++ src/app/calculadora/calculadora.ts
@@ -1,16 +1,36 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-calculadora',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './calculadora.html',
-  styleUrl: './calculadora.css',
+  styleUrl: './calculadora.css'
 })
+export class CalculadoraComponent {
+  numero1: number = 0;
+  numero2: number = 0;
+  resultado: number = 0;
 
-export class Calculadora { 
-numero1 = 0;
-numero2 = 0;
-resultado = 0;
+  somar() {
+    this.resultado = this.numero1 + this.numero2;
+  }
+
+  subtrair() {
+    this.resultado = this.numero1 - this.numero2;
+  }
+
+  multiplicar() {
+    this.resultado = this.numero1 * this.numero2;
+  }
+
+  dividir() {
+    if (this.numero2 !== 0) {
+      this.resultado = this.numero1 / this.numero2;
+    } else {
+      alert('Não é possível dividir por zero!');
+    }
+  }
 }
